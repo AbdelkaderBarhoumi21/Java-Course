77d12ddae60ae3144c9961e90413f00d4df5a540
AbdelkaderBarhoumi21 <abdelkaderbarhoumi21@gmail.com>
2026-07-17 22:33:12 +0100

    feat(project): add Order record for managing order details and JSON serialization

---
+++ java_course/src/projects/OrderSystem/Domain/Order.java
@@ -0,0 +1,31 @@
+package Domain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+record Order(
+        OrderId id,
+        List<CartItem> items,
+        double subtotal,
+        double discountAmount,
+        double total,
+        String placedAt) {
+    String toJson() {
+        String itemJson = items.stream().map(
+                item -> """
+                        {"product": "%s","qty": %d,"subtotal": %.2f}
+                        """.formatted(item.product().name(), item.quantity(), item.subTotal()))
+                .collect(Collectors.joining(",\n ", "[\n ", "\n ]"));
+        return """
+                {
+                   "id": "%s",
+                   "items":"%s",
+                   "subtotal": %.2f,
+                   "discountAmount": %.2f,
+                   "total": %.2f,
+                   "placedAt":"%s"
+                }
+                        """.formatted(id.value(), itemJson, subtotal, discountAmount, total, placedAt);
+
+    }
+}
