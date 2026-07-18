dfa65da46ba098f6ee2e0acce7e25316ae97b2c0
AbdelkaderBarhoumi21 <abdelkaderbarhoumi21@gmail.com>
2026-07-18 12:13:54 +0100

    refactor(Order): update Order record and toJson method for improved readability

--- java_course/src/projects/OrderSystem/Domain/Order.java
+++ java_course/src/projects/OrderSystem/Domain/Order.java
@@ -3,29 +3,29 @@ package Domain;
 import java.util.List;
 import java.util.stream.Collectors;
 
-record Order(
-        OrderId id,
-        List<CartItem> items,
-        double subtotal,
-        double discountAmount,
-        double total,
-        String placedAt) {
-    String toJson() {
-        String itemJson = items.stream().map(
-                item -> """
-                        {"product": "%s","qty": %d,"subtotal": %.2f}
-                        """.formatted(item.product().name(), item.quantity(), item.subTotal()))
-                .collect(Collectors.joining(",\n ", "[\n ", "\n ]"));
-        return """
-                {
-                   "id": "%s",
-                   "items":"%s",
-                   "subtotal": %.2f,
-                   "discountAmount": %.2f,
-                   "total": %.2f,
-                   "placedAt":"%s"
-                }
-                        """.formatted(id, itemJson, subtotal, discountAmount, total, placedAt);
+public record Order(
+                OrderId id,
+                List<CartItem> items,
+                double subtotal,
+                double discountAmount,
+                double total,
+                String placedAt) {
+        public String toJson() {
+                String itemJson = items.stream().map(
+                                item -> """
+                                                {"product": "%s","qty": %d,"subtotal": %.2f}
+                                                """.formatted(item.product().name(), item.quantity(), item.subTotal()))
+                                .collect(Collectors.joining(",\n ", "[\n ", "\n ]"));
+                return """
+                                {
+                                   "id": "%s",
+                                   "items":"%s",
+                                   "subtotal": %.2f,
+                                   "discountAmount": %.2f,
+                                   "total": %.2f,
+                                   "placedAt":"%s"
+                                }
+                                        """.formatted(id, itemJson, subtotal, discountAmount, total, placedAt);
 
-    }
+        }
 }
